e47d74a99345d5257c22ca0258508bfb592dd4bd
Muhammad-Raza156 <hafizrazam@gmail.com>
2024-10-25 13:12:31 +0500

    Udated  files added

--- app/src/database.py
+++ app/src/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import  create_engine, Session
 from config import database_url
 
 engine=create_engine(database_url, echo=True)
--- app/src/domain/user/service.py
+++ app/src/domain/user/service.py
@@ -1,4 +1,38 @@
-from schemas import UserCreate
+from schemas import UserCreate, UserLogin
 from app.src.dependencies import SessionDep
+from app.src.database import engine
+from sqlmodel import SQLModel, select
+from app.src.domain.user.models import User
+from fastapi import HTTPException, status
 
-async def create_user(user:UserCreate):
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+#session=SessionDep
+async def create_user(user:UserCreate, session:SessionDep):
+    async with session:
+        query = select(User).where(User.username == user.username)
+        existing_user = await session.exec(query)
+        user_found = existing_user.first()
+        if user_found:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username  already exists")
+        new_user = User(username=user.username, hashed_password= user.password, email=user.email)
+        session.add(new_user)
+
+        await session.commit()
+
+        await session.refresh(new_user)
+    return new_user
+
+async def authenticate_user(userlogin:UserLogin, session:SessionDep):
+    async with session:
+        query = select(User).where(User.username == userlogin.username & User.hashed_password==userlogin.password)
+        result = await session.exec(query)
+        user= result.first()
+        if user:
+            return {"message": "user validated"}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+
+
