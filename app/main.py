75801bce16b45df8b351b1dfc3243c1110375996
codwithabid <codwithabid@gmail.com>
2024-10-31 14:26:06 +0500

    Updatted

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import os
+from .src.database import database, create_db
 from app.src.database import database, create_db, database_url
 from app.src.routers.buyers import router as buyers_router
-import os
+from .src.routers.buyers import router as buyers_router
+from .src.routers.sellers import router as sellers_router
+from .src.routers.sales import router as sales_router
+from .src.routers.auth import router as auth_router
 
 app = FastAPI()
 
@@ -13,7 +18,8 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-# Include your routers here, as
+# Include routers
+app.include_router(auth_router, tags=["User Registration & Login"])
+app.include_router(sellers_router)
 app.include_router(buyers_router)
-
+app.include_router(sales_router)
